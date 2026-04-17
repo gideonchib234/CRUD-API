b59d7faba2bf4648d93070f827e5832955d05278
Gideon ugwu <gideonchib234@gmail.com>
2026-04-17 08:04:52 +0100

    updated url string

--- src/Database/db.js
+++ src/Database/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGO_URI = process.env.MONGO_URI || '';
+const MONGO_URI = process.env.MONGO_URI
 
-mongoose.set('strictQuery', false);
-
+mongoose.set('strictQuery', true);
 const connectDb = async () => {
   try {
     await mongoose.connect(MONGO_URI);
+
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
